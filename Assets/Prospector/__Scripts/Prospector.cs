1b18d0837835a97f12584657434b610219d95104
Aasim040 <aasim040@gmail.com>
2022-03-31 16:57:13 -0400

    Took out a line

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -73,7 +73,7 @@ public class Prospector : MonoBehaviour
     void Start()
     {
 
-        Scoreboard.S.score = ScoreManager.SCORE;
+
         deck = GetComponent<Deck>();
         deck.InitDeck(deckXML.text);
         Deck.Shuffle(ref deck.cards);
